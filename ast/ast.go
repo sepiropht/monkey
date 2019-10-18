110a9565c02f831970444b6ce9374504b08a8709
William Mbotta <sergembotta@mailoo.org>
2019-10-18 11:35:03 +0200

    chap 2.5

--- ast/ast.go
+++ ast/ast.go
@@ -33,6 +33,14 @@ type LetStatement struct {
 	Value Expression
 }
 
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
