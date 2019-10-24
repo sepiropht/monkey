3a323d9da45095d5e56420ef71b87adc9367aa58
William Mbotta <sergembotta@mailoo.org>
2019-10-24 16:59:53 +0200

    RPPL

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
 // PROMPT begin off each line
@@ -22,11 +22,57 @@ func Start(in io.Reader, out io.Writer) {
 			return
 		}
 		line := scanner.Text()
-
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+const MONKEY_FACE = `
+.--.
+.-"
+/ .. \/
+| | '|
+| \ \
+"-.
+.-. .-.
+/
+Y
+__,__
+.--.
+\/ .. \
+\
+\ 0 | 0 /
+|' | |
+/ / |
+\ '- ,\.-"""""""-./, -' /
+''-' /_
+|
+^ ^
+\._
+\
+_\ '-''
+_./
+|
+\ '~' /
+/
+'._ '-=-' _.'
+'-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
