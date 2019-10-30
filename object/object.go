c63304e315131b49d064f469e3a1b21976debba8
William Mbotta <sergembotta@mailoo.org>
2019-10-30 17:18:54 +0100

    4.3

--- object/object.go
+++ object/object.go
@@ -31,6 +31,7 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Boolean struct {
@@ -85,3 +86,12 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
+
+type BuiltinFunction func(args ...Object) Object
